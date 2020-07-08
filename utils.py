be3fbc282280e2352af736e9c74f9f1205f6234d
Yanik-Pascal Foerster <yanik-pascal.foerster@kcl.ac.uk>
2020-07-08 18:53:30 +0100

    Count_duplicates is now method of patternWalker. Migrated calculation of stationary distribution, normalised laplacian and mfpts to utils.py. Added docstrings for all of the latter.

--- utils.py
+++ utils.py
@@ -9,7 +9,10 @@ leaves-- Return nodes with out-degree zero of given graph.
 uniform_ditree-- Draw uniform random tree and directify.
 downward_current-- Calculate downward two-step probabilites through given nodes.
 upward_current-- Calculate upward two-step probabilites through given nodes.
-
+path_direction_profile-- Return list of step directions for a path and reference.
+largest_eigenvector-- Return the dominant eigenvector of a Markov chain.
+normalised_laplacian-- Return normalised_laplacian Laplacian of a Markov chain.
+mfpt-- Return mean first passage times between set of Markov chain states.
 
 # NOTE: The following five functions were introduced to explore a certain
 concepts and are not employed at the moment.
@@ -117,14 +120,20 @@ def downward_current(G,prob_dict,nodelist):
     children are determined based on G.
 
     G-- networkx.Digraph. Does not neccessarily need to be arborescent.
+        NOTE! For certain examples (like random_walker.patternWalker),
+        G might not be the graph on which a random walker with these two-step
+        probabilites treads. The reason is that introducing probabilites for
+        stepping upwards changes the notion of "children" and "parents". Make
+        sure that the input graph G reflects the desired hierarchy.
     prob_dict-- Edge data for nodes in G; interpreted as probabilites.
     nodelist-- The nodes in G to consider.
+
     return-- list of two-step probabilities.
     """
     out=[]
     for site in nodelist:
-        parents=self.hierarchy_backup.predecessors(site)
-        children=self.hierarchy_backup.successors(site)
+        parents=G.predecessors(site)
+        children=G.successors(site)
         #Note that the formula allows for more than one parent.
         out.append(
             np.sum([self.edges[site,parent]['prob'] for parent in parents])*\
@@ -140,20 +149,161 @@ def upward_current():
     children are determined based on G.
 
     G-- networkx.Digraph. Does not neccessarily need to be arborescent.
+        NOTE! For certain examples (like random_walker.patternWalker),
+        G might not be the graph on which a random walker with these two-step
+        probabilites treads. The reason is that introducing probabilites for
+        stepping upwards changes the notion of "children" and "parents". Make
+        sure that the input graph G reflects the desired hierarchy.
     prob_dict-- Edge data for nodes in G; interpreted as probabilites.
     nodelist-- The nodes in G to consider.
+
     return-- list of two-step probabilities.
     """
     out=[]
     for site in nodelist:
-        parents=self.hierarchy_backup.predecessors(site)
-        children=self.hierarchy_backup.successors(site)
+        parents=G.predecessors(site)
+        children=G.successors(site)
         #Note that the formula allows for more than one parent.
         out.append(
             np.sum([self.edges[parent,site]['prob'] for parent in parents])*\
             np.sum([self.edges[site,child]['prob'] for child in children])
             )
     return out
+
+def path_direction_profile(G,reference,path):
+    """
+    For each node in path, decide whether the distance to the reference has
+    increased or decreased.
+
+    G-- nx.Graph or anothing compatible with nx.shortest_path_length.
+    reference-- The based on which 'direction' is defined. A step that
+        decreases the distance to reference is assigned -1, else +1.
+    path-- Iterable of nodes, representing a path. Does not neccessarily need
+        to be an list of successively adjecent nodes.
+
+    return-- list of +1 and -1, indicating the direction of each step. Note
+        that len(return)=len(path)-1.
+    """
+    return [
+            -1 if
+            nx.shortest_path_length(self,path[i+1],reference)>
+            nx.shortest_path_length(self,path[i],reference)
+            else 1
+            for i in range(len(path[:-1]))
+            ]
+
+def largest_eigenvector(G,weight='weight'):
+    """
+    Return the maximum eigenvector of a Markov chain with state space G.
+
+    G-- nx.Graph
+    weight-- Name of the edge attibute to use in the transition matrix of the
+        Markov chain. Treat matrix entry [i,j] as the transition probability
+        from i to j, and assume it to be normalised. (default: 'weight')
+
+    return-- Left eigenvector of transition matrix corresponding to eigenvalue
+        with greatest real part. Normalise entries to sum up to unity.
+    """
+    #trans_{i,j}=Prob(j|i)= Prob(i->j)
+    trans = nx.to_numpy_matrix(G,weight=weight)
+    evls, evcs = np.linalg.eig(trans.T)
+    max_evl_ndx=np.argmax(np.real(evls))
+    #normalise entries to sum up to 1
+    evcs[:,max_evl_ndx]/=np.sum(evcs[:,max_evl_ndx])
+    #If looking for stationary distribution, entries must be non-negative
+    if any( [x<0 for x in evcs[:,max_evl_ndx]] ):
+        evcs[:,max_evl_ndx]*=-1
+    return np.squeeze(np.array(evcs[:,max_evl_ndx]))
+
+def normalised_laplacian(G,weight='weight',stat_dist=None):
+    """
+    Return the normalised Laplacian of a Markov chain with state space G.
+
+    G-- nx.Graph
+    weight-- Name of the edge attibute to use in the transition matrix of the
+        Markov chain. Treat matrix entry [i,j] as the transition probability
+        from i to j, and assume it to be normalised. (default: 'weight')
+    stat_dist-- Stationary distribution of the Markov chain (left eigenvector
+        for the unit eigenvalue of the transition matrix). If None, calculate
+        using largest_eigenvector.
+
+    return-- Normalised Laplacian, np.ndarray with shape (len(G),len(G)).
+    """
+    pi=stat_dist
+    if pi is None:
+        pi=largest_eigenvector(G,weight=weight)
+    trans=nx.to_numpy_matrix(G, weight=weight)
+    return np.matmul(np.matmul(np.diagflat(np.sqrt(pi)),np.eye(len(pi))-trans),np.diagflat(1/np.sqrt(pi)))
+
+def mfpt(G,node_pairs,weight='weight',stat_dist=None,norm_laplacian=None,
+        method='fundamental_matrix'
+        ):
+    """
+    Return mean first passage times (MFPT) of a Markov chain on G
+    for list of pairs of nodes.
+
+    G-- nx.Graph.
+    node_pairs-- Iterateable with node tuples (start_node,target_node).
+    weight-- Name of the edge attibute to use in the transition matrix of the
+        Markov chain. Treat matrix entry [i,j] as the transition probability
+        from i to j, and assume it to be normalised. (default: 'weight')
+    stat_dist-- Stationary distribution of the Markov chain (left eigenvector
+        for the unit eigenvalue of the transition matrix). If None, calculate
+        using largest_eigenvector.
+    norm_laplacian-- Normalised Laplacian of the Markov chain. If None,
+        calculate using norm_laplacian.
+        (Only needed for method='fundamental_matrix')
+    method-- Either 'fundamental_matrix' (default) or 'eig'. The former uses
+        the (pseudo)inverse of the normalised Laplacian
+        (see Yanhua & Zhang, 2010) the latter eigenvector decomposition of the
+        transition matrix. Note that the letter requires DB to be correct, and
+        NOTE THAT THE LATTER METHOD IS FAULTY AT THE MOMENT.
+
+    return-- If node_pairs contains only one tuple (start,target):
+        MFPT between start and target.
+        Else: Dict of dicts of MFPTs keyed by the nodes in node_pairs.
+    """
+    out={{}}
+    if method=='fundamental_matrix'
+        trans=nx.to_numpy_matrix(G, weight=weight)
+        pi=stat_dist
+        nlap=norm_laplacian
+        if pi is None:
+            pi=largest_eigenvector(G,weight=weight)
+        if norm_laplacian is None:
+            norm_laplacian=normalised_laplacian(G,weight,pi)
+        inv_nlap=np.linalg.pinv(norm_laplacian) #pseudoinverse of nlap
+        for pair in node_pairs:
+            start_ndx=list(G.nodes()).index(pair[0])
+            target_ndx=list(G.nodes()).index(pair[1])
+            out[pair[0]][pair[1]]=inv_lap[start_ndx,start_ndx]/pi[start_ndx]-\
+                inv_lap[target_ndx,start_ndx]/np.sqrt(pi[start_ndx]*pi[target_ndx])
+
+    if method=='eig':
+        trans = nx.to_numpy_matrix(G,weight=weight).T #trans_{i,j}=Prob(i|j)= Prob(j->i)
+        evls, r_evcs = np.linalg.eig(trans)
+        max_evl_ndx=np.argmax(np.real(evls))
+        r_evcs[:,max_evl_ndx]/=np.sum(r_evcs[:,max_evl_ndx])
+        if any( [x<0 for x in r_evcs[:,max_evl_ndx]] ):
+            r_evcs[:,max_evl_ndx]*=-1
+        p_eq=r_evcs[:,max_evl_ndx]
+        l_evcs=np.linalg.inv(r_evcs)
+        root_ndx=list(G.nodes()).index(self.root)
+        target_ndx=list(G.nodes()).index(self.target_node)
+        for pair in node_pairs:
+            start_ndx=list(G.nodes()).index(pair[0])
+            target_ndx=list(G.nodes()).index(pair[1])
+            out[pair[0]][pair[1]]=1/p_eq[start_ndx]*(
+                1+ np.sum([
+                    evls[l]/(1-evls[l])*r_evcs[start_ndx,l]*\
+                    (l_evcs[start_ndx,l]-l_evcs[target_ndx,l])
+                    for l in range(len(evls)) if l != max_evl_ndx
+                    ])
+                )
+    if len(node_pairs)==1:
+        return out[node_pairs[1][0]][node_pairs[0][1]]
+    else:
+        return out
 
 
 def above(G,x):
