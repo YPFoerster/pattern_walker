48fc608654ca2cbbc7ec47f6954937ae57bd3897
Yanik-Pascal Foerster <k1801311@kcl.ac.uk>
2020-10-18 08:40:26 +0100

    Made scheduler instructions more precise.

--- job_submit.py
+++ job_submit.py
@@ -5,7 +5,8 @@ Adapted from https://vsoch.github.io/lessons/sherlock-jobs/
 
 import os
 import subprocess
-
+from itertools import product
+import numpy as np
 
 def mkdir_p(dir):
     '''make a directory (dir) if it doesn't exist'''
@@ -13,7 +14,7 @@ def mkdir_p(dir):
         os.mkdir(dir)
 
 
-project_name='fractional_overlaps'
+project_name='combi_overlap_gamma_section_0'
 project_dir='{}/{}'.format(os.getcwd(),project_name)
 
 mkdir_p(project_dir)
@@ -34,10 +35,16 @@ mkdir_p(data_dir)
 #    'n_cores':4},{'job_name':'fpt_histogram_5','r':3,'h':4,'gamma':0.3,'N':15,'n_samples':100000,
 #    'n_cores':4}]
 
-overlap_range=[0.2,0.4,0.6,0.8]
+N=15
+cores=2
+overlap_range=np.arange(0,0.6,0.01)#[x*0.05 for x in range(21) ]
+gamma_range=np.arange(0.45,0.91,0.01)#[x*0.05 for x in range(21)]
+param_range=list(product(gamma_range,overlap_range))
+n_jobs=len(param_range)
+param_range=[(num,*param_range[num]) for num in range(n_jobs) ]
 job_params_dicts=[
-    {'job_name':'{project_name}_{job_num}'.format(project_name=project_name,job_num=job_num),'r':2,'seed':0,'gamma':1/6.,'N':15,'overlap':overlap ,'n_samples':10,
-    'n_cores':4} for (job_num,overlap) in zip(range(len(overlap_range)),overlap_range)
+    {'job_name':'{project_name}_{job_num}'.format(project_name=project_name,job_num=job_num),'r':2,'seed':0,'gamma':gamma,'N':N,'overlap':overlap ,'n_samples':10000,
+    'n_cores':cores} for (job_num,gamma,overlap) in param_range
 ]
 
 
@@ -57,12 +64,15 @@ for job in jobs:
         fh.writelines("#!/bin/bash -l\n")
         fh.writelines("#SBATCH --partition=nms_research\n")
         fh.writelines("#SBATCH --job-name={}.job\n".format(job["job_name"]))
+        #fh.writelines("#SBATCH --workdir={}\n".format(project_dir))
         fh.writelines("#SBATCH --output={}\n".format(stdout_file))
         fh.writelines("#SBATCH --error={}\n".format(err_file))
-        fh.writelines("#SBATCH --time=0-00:05\n")
-        fh.writelines("#SBATCH --mem=1200\n")
+        fh.writelines("#SBATCH --time=0-01:00\n")
+        fh.writelines("#SBATCH --mem=500\n")
+        fh.writelines("#SBATCH --nodes=1\n")
+        fh.writelines("#SBATCH --ntasks={}\n".format(cores))
         fh.writelines("module load devtools/anaconda\n")
-        fh.writelines("python fractional_overlap_test.py \
+        fh.writelines("python /users/k1801311/asymmetric_random_walker/fractional_overlap_test.py \
             --lam {r} --seed {seed} --gamma {gamma} --string-len {N} --overlap {overlap}\
             --num-samples {n_samples} --num-cores {n_cores} --job-id {id} \
             --job-name {job_name} --output-dir {out_dir}\
