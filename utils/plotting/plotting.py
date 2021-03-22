444111422eb9b518a655f5b42449f27725cbc06d
Yanik-Pascal Foerster <yanik-pascal.foerster@kcl.ac.uk>
2021-03-22 10:57:02 +0000

    allowed to pass a list of nodes to create a visual "partition"

--- utils/plotting/plotting.py
+++ utils/plotting/plotting.py
@@ -7,12 +7,12 @@ __all__ = [
     'plot_tree'
     ]
 
-def plot_tree(G):
+def plot_tree(G,partition=[]):
     pos=graphviz_layout(G.hierarchy_backup,prog='dot') #positions to display tree "nicely"
     (edges,weights) = zip(*nx.get_edge_attributes(G,'weight').items())
     fig_handle=plt.figure(figsize=(40,20))
-    nx.draw(G, pos, edgelist=edges, edge_color=weights, node_size=5000,arrowsize=100,width=5.0, edge_cmap=plt.cm.Blues)
-    nx.draw_networkx_nodes(G,pos,nodelist=[G.root],node_color='r',node_size=5000.)
-    #Mark target node in green.
-    nx.draw_networkx_nodes(G,pos,nodelist=[G.target_node],node_color='g',node_size=5000)
+    nx.draw(G, pos, edgelist=edges, edge_color=weights, node_size=3000,arrowsize=100,width=5.0, edge_cmap=plt.cm.Blues) #edge colour according to weight
+    nx.draw_networkx_nodes(G,pos,nodelist=[G.root],node_color='r',node_size=5000.) #root in red
+    nx.draw_networkx_nodes(G,pos,nodelist=[G.target_node],node_color='g',node_size=5000) #target in green
+    nx.draw_networkx_nodes(G,pos,nodelist=partition,node_size=6000,node_shape='h',linewidths=10,edgecolors='k')
     return fig_handle
