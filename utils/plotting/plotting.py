72343ea5e8906f9e40746243662e60b0e58447d3
Yanik-Pascal Foerster <yanik-pascal.foerster@kcl.ac.uk>
2022-11-18 14:13:10 +0000

    add linewidth for symbols on case circles are not filled for some reason

--- utils/plotting/plotting.py
+++ utils/plotting/plotting.py
@@ -13,7 +13,7 @@ def plot_tree(G,partition=[]):
     fig_handle=plt.figure(figsize=(40,20))
     nx.draw(G, pos, edgelist=edges, edge_color=weights, node_size=3000,arrowsize=100,width=5.0, edge_cmap=plt.cm.Blues) #edge colour according to weight
     nx.draw_networkx_nodes(G,pos,nodelist=partition,node_size=5000,node_shape='h',linewidths=10,edgecolors='k')
-    nx.draw_networkx_nodes(G,pos,nodelist=[G.root],node_color='r',node_size=3000.) #root in red
-    nx.draw_networkx_nodes(G,pos,nodelist=[G.target_node],node_color='g',node_size=3000) #target in green
+    nx.draw_networkx_nodes(G,pos,nodelist=[G.root],node_color='r',linewidths=10,node_size=3000.) #root in red
+    nx.draw_networkx_nodes(G,pos,nodelist=[G.target_node],node_color='b',linewidths=10,node_size=3000) #target in blue
 
     return fig_handle,pos
