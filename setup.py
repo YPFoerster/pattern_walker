21e5ba487aec1695851b84498fd705cb6a83c0d2
Yanik-Pascal Foerster <yanik-pascal.foerster@kcl.ac.uk>
2022-12-16 16:58:09 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+
+setup(name='pattern_walker',
+      version='1.0',
+      description='A random walker model for browsing the law.',
+      url='http://github.com/YPFoerster/pattern_walker',
+      author='Yanik Foerster',
+      author_email='ypfoerster@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
