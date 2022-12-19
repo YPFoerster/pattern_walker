8a71d3e79644542e78345fe9ecfb909fe364f039
Yanik-Pascal Foerster <yanik-pascal.foerster@kcl.ac.uk>
2022-12-19 09:16:34 +0000

    update version

--- pattern_walker/__init__.py
+++ pattern_walker/__init__.py
@@ -1,1 +1,3 @@
+__version__= "2.1"
+
 from pattern_walker.random_walker import *
--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup,find_packages
 
+def get_version(fp) -> str:
+    with open(fp, "r") as f:
+        for line in f:
+            if "version" in line:
+                delim = '"'
+                return line.split(delim)[1]
+    raise RuntimeError(f"could not find a valid version string in {fp}.")
+
 
 setup(name='pattern_walker',
-      version='2.0',
+      version=get_version('pattern_walker/__init__.py'),
       description='A random walker model for browsing the law.',
       url='http://github.com/YPFoerster/pattern_walker',
       author='Yanik Foerster',
